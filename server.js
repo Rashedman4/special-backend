9d72f4835a7ab9da1f08f71200771eafa4891e1c
Rashed Almanasrah <rashed.almanasah@yahoo.com>
2026-01-19 14:32:22 +0300

    admin api commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import dotenv from "dotenv";
 import db from "./db.js";
 import authRoutes from "./routes/auth.js";
 
+import adminOverviewRoutes from "./routes/admin/overview.js";
+import adminUsersRoutes from "./routes/admin/users.js";
+import adminCommunitiesRoutes from "./routes/admin/community.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
+
+app.use("/api/admin/overview", adminOverviewRoutes);
+app.use("/api/admin/users", adminUsersRoutes);
+app.use("/api/admin/community", adminCommunitiesRoutes);
 app.get("/", (req, res) => res.send("TokenSphere API running"));
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
